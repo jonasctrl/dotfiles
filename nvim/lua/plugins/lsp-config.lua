d4a17b281c32bcf469fb138726b9a5dde4150f6d
Jonas Gaigalas <jonas.gaigalas@adv.bet>
2025-07-25 20:55:39 +0300

    Update lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,131 +1,98 @@
 return {
-    -- Mason for managing LSP servers
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls", "ts_ls", "pyright" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
     },
-    -- Mason LSP Config
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "gopls", "ts_ls", "pyright" },
-                automatic_installation = true,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+      end
+
+      local mason_lspconfig = require("mason-lspconfig")
+
+      if mason_lspconfig.setup_handlers then
+        mason_lspconfig.setup_handlers({
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
             })
+          end,
+        })
+      else
+        local servers = { "lua_ls", "gopls", "ts_ls", "pyright" }
+        for _, server in ipairs(servers) do
+          lspconfig[server].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+          })
         end
+      end
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
     },
-    -- Simple completion
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
         },
-        config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                }),
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "buffer" },
-                    { name = "path" },
-                },
-            })
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports", "gofmt" },
+        lua = { "stylua" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+      },
+    },
+    config = function(_, opts)
+      require("conform").setup(opts)
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function(args)
+          require("conform").format({ bufnr = args.buf })
         end,
-    },
-    -- Modern formatting
-    {
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup({
-                formatters_by_ft = {
-                    go = { "goimports" },
-                    lua = { "stylua" },
-                    python = { "black" },
-                    javascript = { "prettier" },
-                    typescript = { "prettier" },
-                    vue = { "prettier" },
-                },
-            })
-
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.go",
-                callback = function(args)
-                    require("conform").format({
-                        bufnr = args.buf,
-                        lsp_fallback = true,
-                        async = true,
-                    })
-                end,
-            })
-        end
-    },
-    -- LSP configuration
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "mason.nvim",
-            "mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            local on_attach = function(_, bufnr)
-                local opts = { buffer = bufnr, silent = true }
-                -- Essential keymaps
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "<leader>f", function()
-                    require("conform").format({ lsp_fallback = true, async = true })
-                end, opts)
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-                -- F2 for fast renaming
-                vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-            end
-
-            -- Setup servers
-            local servers = {
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            diagnostics = { globals = { "vim" } },
-                            telemetry = { enable = false },
-                        },
-                    },
-                },
-                gopls = {
-                    settings = {
-                        gopls = {
-                            gofumpt = true,
-                            completeUnimported = true,
-                            usePlaceholders = true,
-                            analyses = {
-                                unusedparams = true,
-                            },
-                        },
-                    },
-                },
-                ts_ls = {},
-                pyright = {},
-            }
-
-            for server, config in pairs(servers) do
-                lspconfig[server].setup(vim.tbl_deep_extend("force", {
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                }, config))
-            end
-        end,
-    },
+      })
+    end,
+  },
 }
