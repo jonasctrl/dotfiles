542368e8d0a92179a62e0c16886df7ee363967cf
Jonas Gaigalas <jonas.gaigalas@adv.bet>
2025-11-15 23:42:43 +0200

    improvements

--- nvim/lua/plugins/conflict.lua
+++ nvim/lua/plugins/conflict.lua
@@ -8,7 +8,6 @@ return {
         vim.keymap.set('n', '<leader>ct', '<Plug>(git-conflict-theirs)', { desc = 'Conflict: Choose theirs' })
         vim.keymap.set('n', '<leader>cb', '<Plug>(git-conflict-both)', { desc = 'Conflict: Choose both' })
         vim.keymap.set('n', '<leader>c0', '<Plug>(git-conflict-none)', { desc = 'Conflict: Choose none' })
-
         vim.keymap.set('n', ']x', '<Plug>(git-conflict-next-conflict)', { desc = 'Conflict: Next conflict' })
         vim.keymap.set('n', '[x', '<Plug>(git-conflict-prev-conflict)', { desc = 'Conflict: Previous conflict' })
     end,
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -21,9 +21,24 @@ return {
                 map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
                 map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
                 map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle blame")
-                -- map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-                -- map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+                map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+                map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
                 map("n", "<leader>hd", gs.diffthis, "Diff this")
+
+                -- custom mapping to open the commit in browser using snacks.nvim
+                map("n", "<leader>ho", function()
+                    local line = vim.api.nvim_win_get_cursor(0)[1]
+                    local cmd = string.format('git blame -L %d,%d --porcelain %s',
+                        line, line,
+                        vim.fn.shellescape(vim.fn.expand('%')))
+                    local output = vim.fn.system(cmd)
+                    local commit = output:match("^(%x+)")
+                    if commit and commit ~= "" and commit ~= "0000000000000000000000000000000000000000" then
+                        require("snacks").gitbrowse({ commit = commit })
+                    else
+                        vim.notify("No commit found for this line", vim.log.levels.WARN)
+                    end
+                end, "Open commit in browser")
             end
         })
 
