81e8f87430867291b46632c0614e25db6000ca9f
Jonas Gaigalas <jonas.gaigalas@adv.bet>
2025-11-18 18:54:20 +0200

    consolidate tree sitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,82 @@
 -- NOTE: Setup treesitter for various languages
 -- Highlight, indentation, autotagging
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "windwp/nvim-ts-autotag",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "vue",
+                    "html",
+                    "css",
+                    "json",
+                    "python",
+                    "go",
+                    "gomod",
+                    "gowork",
+                    "gosum",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "yaml",
+                    "toml",
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                    disable = { "python" },
+                },
+                -- NOTE: This requires nvim-ts-autotag
+                autotag = {
+                    enable = true,
+                    enable_close_on_slash = false,
+                },
+            })
+        end,
     },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "vue",
-                "html",
-                "css",
-                "json",
-                "python",
-                "go",
-                "gomod",
-                "gowork",
-                "markdown",
-                "markdown_inline",
-                "bash",
-                "yaml",
-                "toml",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-            autotag = { enable = true },
-        })
-    end,
+
+    -- NOTE: Show code context at the top of the window
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("treesitter-context").setup({
+                enable = true,
+                multiwindow = false,
+                max_lines = 0,
+                min_window_height = 0,
+                line_numbers = true,
+                multiline_threshold = 20,
+                trim_scope = 'outer',
+                mode = 'cursor',
+                separator = nil,
+                zindex = 20,
+                on_attach = nil,
+            })
+        end,
+    }
 }
